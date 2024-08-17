283495925440f7a60d4a09c415f86b15b24a9002
amxamxa <max.kempter@gmail.com>
2024-08-17 13:43:49 +0200

    major update mit Unterordnern

---
+++ functions/my-functions.zsh
@@ -0,0 +1,132 @@
+#!/usr/bin/env zsh
+#########################################
+## ╔═╗╦╦  ╔═╗           ________________
+## ╠╣ ║║  ║╣     
+## ╚  ╩╩═╝╚═╝           ''''''''''''''''
+## 	 -NAME:  	my-functions.zsh 
+##	 -PATH:    	$ZDOTDIR/functions
+##	 -STATUS:	work in progress
+##	 -USAGE:	
+## -------------------------------------
+# FILE DATES	[yyyy-mmm-dd]
+##..SAVE:	  	2024-...
+##..CREATION:   2024-07
+## -----------------------------------------
+## AUTHOR:		mxx
+## COMMENTS: 	
+###########################################
+# 	┌─┐┬ ┬┌┐┌┌─┐┌┬┐┬┌─┐┌┐┌┌─┐
+# zsh	├┤ │ │││││   │ ││ ││││└─┐
+#  	└  └─┘┘└┘└─┘ ┴ ┴└─┘┘└┘└─┘
+###################################calvin
+
+BKP() {
+    for file in "$@"; do
+        # Extrahiere den Dateinamen ohne Pfad und die Erweiterung
+        #  Der "basename" Befehl entfernt den Pfadteil eines Dateipfads und gibt nur den Namen der Datei zurück. Das -- Argument wird verwendet, um sicherzustellen, dass keine weiteren Argumente als Dateinamen interpretiert werden, falls $file mehrere Wörter enthält
+        filename=$(basename -- "$file")
+           #${variable##pattern}: Diese Syntax extrahiert den längsten Teil eines Strings vor dem Muster (pattern). In diesem Fall sucht es nach dem letzten Punkt (.) im Dateinamen und entfernt alles vor diesem Punkt. Da der Punkt selbst nicht in das Ergebnis aufgenommen wird, bleibt nur der Teil nach dem letzten Punkt übrig, also die Dateierweiterung.
+        extension="${filename##*.}"   
+      #${variable%pattern}: Ähnlich wie oben, aber diese Syntax entfernt den kürzesten Teil eines Strings nach dem Muster (pattern). In diesem Fall sucht es nach dem ersten Punkt (.) im Dateinamen und entfernt alles danach, einschließlich des Punktes selbst. Dadurch bleibt nur der Teil des Namens vor dem letzten Punkt übrig, also der Dateiname ohne die Erweiterung.
+      name="${filename%.*}"
+
+        # Erstelle das Backup mit dem aktuellen Datum und behalte die ursprüngliche Erweiterung bei
+        cp -vi "$file" "${name}.$(date +%Y-%m-%d).bkp.${extension}"
+    done
+}
+### -----------------------------------------------------------------------------
+TRASH() {
+    if [ -d "$HOME/.local/share/Trash" ]; then 
+        printf "${GELB}Sind Sie sicher, dass Sie alle Dateien im Trash löschen möchten? (j/n):${RESET} "
+        read answer
+        case $answer in
+            [Jj]* )
+                command rm --recursive --one-file-system --force $HOME/.local/share/Trash/*
+                echo -e "${GREEN}Alles gelöscht.${RESET}"
+                ;;
+            * )
+                echo -e "${YELLOW}Aktion abgebrohen.${RESET}"
+                ;;
+        esac
+    else 
+        echo -e "${YELLOW}Trash-Verzeichnis existiert nicht.${RESET}"
+    fi
+}
+# nachfolgend Comment, da Syntax Bash-Funktion ist	
+# TRASH() {
+# 	    if [ -d "$HOME/.local/share/Trash" ]; then 
+# 	        read -rp "Sind Sie sicher, dass Sie alle Dateien im Trash löschen möchten? (j/n): " answer
+# 	        case $answer in
+# 	            [Jj]* )
+# 	                command rm --recursive --one-file-system --force $HOME/.local/share/Trash/*
+# 	                echo -e "${GREEN}Alles gelöscht.${RESET}"
+# 	                ;;
+# 	            * )
+# 	                echo -e "${YELLOW}Aktion abgebrochen.${RESET}"
+# 	                ;;
+# 	        esac
+# 	    else 
+# 	        echo -e "${YELLOW}Trash-Verzeichnis existiert nicht.${RESET}"
+# 	    fi
+## ---------------------------------------------------------------------------------------
+
+## ---------------------------------------------------------------------------------------
+#	   SET RANDOM TERMINAL COLOR THEME
+term-col-theme() {
+  # Überprüfen, ob die Datei existiert
+    if [[ ! -f "$ZDOTDIR/color-theme.md" ]]; then
+        echo "Fehler: Datei $ZDOTDIR/color-theme.md nicht gefunden."
+        return 1
+    fi
+  # Anzahl der Zeilen in .theme_history
+    num_themes=$(wc -l < "$ZDOTDIR/color-theme.md")
+  # Generiere eine zufällige Zahl zwischen 1 und der Anzahl der Themes
+    random_num=$((1 + RANDOM % num_themes))
+  # Hole das Theme an der zufälligen Zeilennummer
+    selected_theme=$(sed -n "${random_num}p" < "$ZDOTDIR/color-theme.md")
+  # Setze das ausgewählte Theme
+    echo -e "${PINK}${BLINK} Setting terminal theme to: $selected_theme ${RESET}"
+    command theme.sh $selected_theme
+}
+term-col-theme
+## ---------------------------------------------------------------------------------------
+
+### ------------------------------------------------- ###
+##   Globale Suffix-Aliases erweitern
+### ------------------------------------------------- ###
+globalias() {
+# Aliases erweitern aka sehen kann, mit Leertaste
+	if [[ $LBUFFER =~ ' [A-Z0-9]+$' ]]; then
+		zle _expand_alias
+		zle expand-word
+	fi
+	zle self-insert
+}
+zle -N globalias
+bindkey " " globalias
+# control-space to bypass completion
+bindkey "^ " magic-space   
+# normal space during searches       
+bindkey -M isearch " " magic-space 
+# sonstige Shortcuts in File ~/zsh/shortcuts.zsh
+
+### ------------------------------------------------- ###
+#  	Funktion COPYto .... aliases.maybe  
+#    Speichert den letzten Befehl in die Datei 
+# $ZDOTDIR/aliases.maybe und gibt eine Bestätigungsmeldung aus.
+### ------------------------------------------------- ###
+COPYtoZ() {
+# 'echo $(fc -ln -1)' gibt den letzten Befehl aus.
+# 'tee -a $ZDOTDIR/aliases.maybe' fügt den Ausgabetext an die Datei $ZDOTDIR/aliases.maybe an.
+	echo $(fc -ln -1) | tee -a $ZDOTDIR/aliases.maybe
+# Überprüft, ob der letzte Befehl erfolgreich ausgeführt wurde.
+	if [ $? -eq 0 ]; then
+# Wenn der Befehl erfolgreich ausgeführt wurde, gibt diese Funktion eine Bestätigungsmeldung aus.
+	  echo "${ROSA}Befehl $(fc -ln -1) wurde erfolgreich an $ZDOTDIR/aliases.maybe angehängt.${RESET}"
+	else
+# Wenn der Befehl fehlgeschlagen ist, gibt diese Funktion eine Fehlermeldung aus.
+	echo "${ROSA}Fehler beim Anhängen des Befehls $(fc -ln -1) an $ZDOTDIR/aliases.maybe.${RESET}"
+	fi
+}
+
+
---
+++ functions/zfunctions2.zsh
@@ -0,0 +1,189 @@
+#!/usr/bin/env zsh
+
+#<< 'Explanation'
+###########################################################
+ #Filename   : x                                           #
+# Author     : x                                          #
+# Created    : x                                          #
+# Last edit  : x                                          #
+# Purpose    : x                                          #
+# Reference  : x                                          #
+# Depends    : x                                          #
+# Arguments  : x                                          #
+# Known bugs : x                                          #
+# To do      : x                                          #
+############################################################
+#Explanation
+	
+# imagemagick, cwebp, p7zip
+
+# show the size of images
+imgsize() {
+    for file in "$@"
+    do
+        local width=$(identify -format "%w" "$file")> /dev/null
+        local height=$(identify -format "%h" "$file")> /dev/null
+
+        echo -e "Size of $file: $width*$height"
+    done
+}
+
+############################################################################### Resize an image
+# arg1: the file
+# arg2: the width to resize
+# arg3: modify the file in place
+# Example: imgresize myimage.jpg 780 true
+_imgresize() {
+    local filename=${1%\.*}
+    local extension="${1##*.}"
+    local separator="resize"
+    if [ ! -z $3 ]; then
+        local finalName="$filename.$extension"
+    else
+        local finalName="$filename-$separator$2.$extension"
+    fi
+    convert $1 -quality 100 -resize $2 $finalName
+    echo "$finalName resized to $2"
+}
+
+imgresize() {
+    _imgresize $1 $2 true
+}
+
+# Resize all images
+# arg1: the extension of all images
+# arg2: the width to resize
+# arg3: modify the files in place
+# Example: imgresizeall jpg 780 true
+imgresizeall() {
+    for f in *.${1}; do
+        if [ ! -z $3 ]; then
+            imgresize "$f" ${2} t
+        else
+            imgresize "$f" ${2}
+        fi
+    done
+}
+
+imginvert() {
+    local filename=${1%\.*}
+    local extension="${1##*.}"
+    local separator="-"
+    local invert="inverted"
+    if [ ! -z $2 ]; then
+        local finalName="$filename.$extension"
+    else
+        local finalName="$filename$separator$invert.$extension"
+    fi
+    convert $1 -channel RGB -negate $finalName
+    echo "$finalName inverted"
+}
+
+imgoptimize() {
+    local filename=${1%\.*}
+    local extension="${1##*.}"
+    local separator="-"
+    local suffix="optimized"
+    local finalName="$filename$separator$suffix.$extension"
+    convert $1 -strip -interlace Plane -quality 85% $finalName
+    echo "$finalName created"
+}
+
+_imgoptimize() {
+    local filename=${1%\.*}
+    local extension="${1##*.}"
+    local separator="_"
+    local suffix="optimized"
+    local convert $1 -strip -interlace Plane -quality 85% $1
+    echo "$1 created"
+}
+
+imgoptimizeall() {
+    for f in *.${1}; do
+        _imgoptimize "$f"
+    done
+}
+
+imgtojpg() {
+    for file in "$@"
+    do
+        local filename=${file%\.*}
+        convert -quality 100 $file "${filename}.jpg"
+    done
+}
+
+imgtopng() {
+    for file in "$@"
+    do
+        local filename=${file%\.*}
+        convert -quality 100 $file "${filename}.png"
+    done
+}
+
+imgtowebp() {
+    for file in "$@"
+    do
+        local filename=${file%\.*}
+        cwebp -q 100 $file -o $(basename ${filename}).webp
+    done
+}
+
+##########################################################################
+# Extract files
+extract() {
+    for file in "$@"
+    do
+        if [ -f $file ]; then
+            _ex $file
+        else
+            echo "'$file' is not a valid file"
+        fi
+    done
+}
+
+# Extract files in their own directories
+mkextract() {
+    for file in "$@"
+    do
+        if [ -f $file ]; then
+            local filename=${file%\.*}
+            mkdir -p $filename
+            cp $file $filename
+            cd $filename
+            _ex $file
+            rm -f $file
+            cd -
+        else
+            echo "'$1' is not a valid file"
+        fi
+    done
+}
+
+# Internal function to extract any file
+_ex() {
+    case $1 in
+        *.tar.bz2)  tar xjf $1      ;;
+        *.tar.gz)   tar xzf $1      ;;
+        *.bz2)      bunzip2 $1      ;;
+        *.gz)       gunzip $1       ;;
+        *.tar)      tar xf $1       ;;
+        *.tbz2)     tar xjf $1      ;;
+        *.tgz)      tar xzf $1      ;;
+        *.zip)      unzip $1        ;;
+        *.7z)       7z x $1         ;; # require p7zip
+        *.rar)      7z x $1         ;; # require p7zip
+        *.iso)      7z x $1         ;; # require p7zip
+        *.Z)        uncompress $1   ;;
+        *)          echo "'$1' cannot be extracted" ;;
+    esac
+}
+
+# Compress a file
+# TODO to improve to compress in any possible format
+# TODO to improve to compress multiple files
+compress() {
+    local DATE="$(date +%Y%m%d-%H%M%S)"
+    tar cvzf "$DATE.tar.gz" "$@"
+}
+
+
